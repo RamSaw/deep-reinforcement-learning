07002c8d057225f8d0f92720b7a79161d9b0c892
Mikhail Pravilov <prav.mischa@gmail.com>
2020-02-03 21:11:52 +0300

    Created network for HW02.

--- HW02/agent.py
+++ HW02/agent.py
@@ -1,16 +1,57 @@
-import random
-import numpy as np
 import os
-from .train import transform_state
+import random
+
+import numpy as np
+import torch
+from torch import nn
+
+SEED = 11  # 627, 8, 11
+
+
+def make_reproducible(seed, make_cuda_reproducible):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if make_cuda_reproducible:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+make_reproducible(SEED, make_cuda_reproducible=False)
+
+
+def transform_state(state):
+    return torch.tensor(state)
+
+
+def act(model, state):
+    state = transform_state(state)
+    return torch.argmax(model(state)).item()
 
 
 class Agent:
     def __init__(self):
-        self.model = torch.load(__file__[:-8] + "/agent.pkl")
-        
+        self.model = Agent.generate_model()
+        self.model.load_state_dict(torch.load(__file__[:-8] + "/agent.pkl"))
+        self.model.eval()
+
+    @staticmethod
+    def generate_model():
+        linears = [nn.Linear(8, 128), nn.Linear(128, 64), nn.Linear(64, 4)]
+        #for l in linears:
+        #    nn.init.uniform_(l.weight)
+        #    nn.init.uniform_(l.bias)
+        return nn.Sequential(
+                linears[0],
+                nn.ReLU(),
+                linears[1],
+                nn.ReLU(),
+                linears[2])
+
     def act(self, state):
-        state = transform_state(state)
-        return 0 # TODO
+        with torch.no_grad():
+            return act(self.model, state)
 
     def reset(self):
         pass
--- HW02/train.py
+++ HW02/train.py
@@ -1,29 +1,29 @@
-from gym import make
+import random
+from collections import deque
+
 import numpy as np
 import torch
-import random
+from gym import make
+
+from HW02.agent import Agent, act
 
 N_STEP = 1
 GAMMA = 0.9
 
 
-def transform_state(state):
-    return np.array(state)
-
-
 class DQN:
     def __init__(self, state_dim, action_dim):
         self.gamma = GAMMA ** N_STEP
-        self.model = None # Torch model
+        self.model = Agent.generate_model()
 
     def update(self, transition):
         state, action, next_state, reward, done = transition
 
     def act(self, state, target=False):
-        return 0
+        return act(self.model, state)
 
     def save(self, path):
-        torch.save(self.model, "agent.pkl")
+        torch.save(self.model.state_dict(), "agent.pkl")
 
 
 if __name__ == "__main__":
@@ -31,9 +31,11 @@ if __name__ == "__main__":
     dqn = DQN(state_dim=8, action_dim=4)
     eps = 0.1
     episodes = 1000
+    scores = []
+    best_score = -1000.0
 
     for i in range(episodes):
-        state = transform_state(env.reset())
+        state = env.reset()
         total_reward = 0
         steps = 0
         done = False
@@ -46,7 +48,6 @@ if __name__ == "__main__":
             else:
                 action = dqn.act(state)
             next_state, reward, done, _ = env.step(action)
-            next_state = transform_state(next_state)
             total_reward += reward
             steps += 1
             reward_buffer.append(reward)
@@ -55,10 +56,17 @@ if __name__ == "__main__":
             if len(reward_buffer) == N_STEP:
                 dqn.update((state_buffer[0], action_buffer[0], next_state, sum([(GAMMA ** i) * r for i, r in enumerate(reward_buffer)]), done))
             state = next_state
+            #env.render()
+        scores.append(total_reward)
         if len(reward_buffer) == N_STEP:
             rb = list(reward_buffer)
             for k in range(1, N_STEP):
                 dqn.update((state_buffer[k], action_buffer[k], next_state, sum([(GAMMA ** i) * r for i, r in enumerate(rb[k:])]), done))
 
-        if i % 20 == 0:
-            dqn.save()
+        if (i + 1) % 50 == 0:
+            current_score = np.mean(scores)
+            print(f'Current score: {current_score}')
+            scores = []
+            if current_score > best_score:
+                best_score = current_score
+                dqn.save(__file__[:-8])
