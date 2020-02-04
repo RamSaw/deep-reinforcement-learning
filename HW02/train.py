4242ff482cba08f088eb47be3fbdf7d276ef60b0
Mikhail Pravilov <prav.mischa@gmail.com>
2020-02-04 11:49:05 +0300

    Add memory class

--- HW02/train.py
+++ HW02/train.py
@@ -1,5 +1,5 @@
 import random
-from collections import deque
+from collections import deque, namedtuple
 
 import numpy as np
 import torch
@@ -9,6 +9,30 @@ from HW02.agent import Agent, act
 
 N_STEP = 1
 GAMMA = 0.9
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
 
 class DQN:
