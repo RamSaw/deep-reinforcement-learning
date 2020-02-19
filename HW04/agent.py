ab00792fe960ad757e4f10c32062f1e69cdf7014
Mikhail Pravilov <prav.mischa@gmail.com>
2020-02-19 14:44:05 +0300

    Working version. 1/10

--- HW04/agent.py
+++ HW04/agent.py
@@ -3,17 +3,58 @@ import numpy as np
 import os
 
 import torch
+from torch import nn
 
-from .train import transform_state
+SEED = 423  # 627, 8, 11
+
+
+def make_reproducible(seed, make_cuda_reproducible):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if make_cuda_reproducible:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+make_reproducible(SEED, make_cuda_reproducible=False)
+
+
+def transform_state(state):
+    return torch.tensor(state).float()
+
+
+class Actor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mu_model = nn.Sequential(
+            nn.Linear(26, 64),
+            nn.ReLU(),
+            nn.Linear(64, 6),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        mu = 2 * self.mu_model(x)
+        return mu
 
 
 class Agent:
     def __init__(self):
-        self.model = torch.load(__file__[:-8] + "/agent.pkl")
-        
+        self.actor = Agent.generate_model()
+        self.actor.load_state_dict(torch.load(__file__[:-8] + "/agent.pkl"))
+        self.actor.to(torch.device("cpu"))
+        self.actor.eval()
+
+    @staticmethod
+    def generate_model():
+        return Actor()
+
     def act(self, state):
         state = transform_state(state)
-        return 0  # TODO
+        out = self.actor(state)
+        return out.detach().cpu().numpy()
 
     def reset(self):
         pass
--- HW04/train.py
+++ HW04/train.py
@@ -1,40 +1,42 @@
+import time
 from collections import deque
 
+import pybullet_envs
 from gym import make
 import numpy as np
 import torch
 import random
+
+from HW04.agent import Agent, transform_state
 
 N_STEP = 1
 GAMMA = 0.9
 CLIP = 0.1
 ENTROPY_COEF = 1e-2
 TRAJECTORY_SIZE = 512
-
-
-def transform_state(state):
-    return np.array(state)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class PPO:
     def __init__(self, state_dim, action_dim):
         self.gamma = GAMMA ** N_STEP
-        self.actor = None # Torch model
-        self.critic = None # Torch model
+        self.actor = Agent.generate_model().to(DEVICE)  # Torch model
+        self.critic = None  # Torch model
 
     def update(self, trajectory):
         state, action, rollouted_reward = zip(*trajectory)
-        
+
     def get_value(self, state):
         # Should return expected value of the state
         return 0
 
     def act(self, state):
-        # Remember: agent is not deterministic, sample actions from distribution (e.g. Gaussian)
-        return 0
+        state = transform_state(state)
+        out = self.actor(state)
+        return out.detach().cpu().numpy()
 
-    def save(self, path):
-        torch.save(self.actor, "agent.pkl")
+    def save(self, i):
+        torch.save(self.actor.state_dict(), f'agent_{i}.pkl')
 
 
 if __name__ == "__main__":
@@ -42,29 +44,36 @@ if __name__ == "__main__":
     algo = PPO(state_dim=26, action_dim=6)
     episodes = 10000
 
+    scores = []
+    best_score = -10000.0
+    best_score_intermediate = -10000.0
+    intermediate_i = 10
+    total_steps = 0
+    start = time.time()
+
     reward_buffer = deque()
     state_buffer = deque()
     action_buffer = deque()
     done_buffer = deque()
     for i in range(episodes):
-        state = transform_state(env.reset())
+        state = env.reset()
         total_reward = 0
         steps = 0
         done = False
         while not done:
             action = algo.act(state)
             next_state, reward, done, _ = env.step(action)
-            next_state = transform_state(next_state)
             total_reward += reward
             steps += 1
             reward_buffer.append(reward)
             state_buffer.append(state)
             action_buffer.append(action)
+            done_buffer.append(done)
             if len(action_buffer) == TRAJECTORY_SIZE:
-                rb = list(reward_buffer)
                 rollouted_reward = [algo.get_value(state) if not done else 0]
                 for r, d in zip(reward_buffer, done_buffer):
-                    rollouted_reward.append(r + GAMMA * d * rb[-1])
+                    rollouted_reward.append(
+                        r + GAMMA * d * rollouted_reward[-1])  # TODO: * rb[-1]? rb = list(reward_buffer)
                 rollouted_reward = list(reversed(rollouted_reward))
                 trajectory = []
                 for k in range(0, len(state_buffer)):
@@ -74,6 +83,25 @@ if __name__ == "__main__":
                 reward_buffer.clear()
                 state_buffer.clear()
                 done_buffer.clear()
+            # env.render()
+        scores.append(total_reward)
 
-        if i % 50 == 0:
-            algo.save()
+        if (i + 1) % 50 == 0:
+            current_score = np.mean(scores)
+            print(f'Current score: {current_score}')
+            scores = []
+            if current_score > best_score:
+                best_score = current_score
+                algo.save(50)
+                print(f'Best model saved with score: {best_score}')
+            end = time.time()
+            elapsed = end - start
+            start = end
+            print(f'Elapsed time: {elapsed}')
+        elif (i + 1) % intermediate_i == 0:
+            current_score_intermediate = np.mean(scores[-intermediate_i:])
+            print(f'Intermediate score: {current_score_intermediate}')
+            if current_score_intermediate > best_score_intermediate:
+                best_score_intermediate = current_score_intermediate
+                algo.save(intermediate_i)
+                print(f'Best {intermediate_i} model saved with score: {best_score_intermediate}')
